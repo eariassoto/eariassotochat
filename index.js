1dd22150a51504bac57f4a743ade6ab78b058886
Emmanuel Arias <earias@localhost.localdomain>
2014-10-22 15:48:39 -0600

    pruebas

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var express = require('express')
+//Configuracion de Express
+var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var io = require('socket.io')(server);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+//Establece el puerto
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+server.listen(app.get('port'), function() {
+  console.log("La aplicacion se esta ejecutando en localhost:" + app.get('port'));
+});
